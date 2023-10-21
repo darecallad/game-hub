360eba64244690f2a4724e6c08a34c52d7aab0c8
Johnson <Ashane199080706@hotmail.com.tw>
2023-10-21 13:19:32 -0700

    fetching games using react query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,7 @@
-import apiClient from "../services/api-client";
-import { FetchResponse } from "./useData";
+import apiClient, { FetchResponse } from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+
 interface Platform {
   id: number;
   name: string;
