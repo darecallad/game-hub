e6ac96b76d56584bf10780abf842fcef13a72da5
Johnson <Ashane199080706@hotmail.com.tw>
2023-10-30 21:54:19 -0700

    build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,19 +3,21 @@ import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
 
   if (isLoading) return <Spinner />;
-  if (error) throw error;
+  if (error || !game) throw error;
 
   return (
     <>
       <Heading>{game?.slug}</Heading>
       <ExpandableText>{game?.description_raw || ""}</ExpandableText>
       <GameAttributes game={game!} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
