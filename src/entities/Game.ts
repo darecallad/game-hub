8184d7914f15e16b33cafe2a4a5213a75f43f7c3
Johnson <Ashane199080706@hotmail.com.tw>
2023-10-30 18:29:43 -0700

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,8 +1,12 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   description_raw: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   id: number;
   name: string;
   background_image: string;
